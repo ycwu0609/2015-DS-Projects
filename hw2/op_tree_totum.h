8b7918a86055e679e1363d93d2360f73c09fd8ed
ycwu0609 <winie850609@gmail.com>
2017-04-15 20:34:55 +0800

    Add files via upload

---
+++ hw2/op_tree_totum.h
@@ -0,0 +1,109 @@
+#include<iostream>
+using namespace std;
+
+class node{
+public:
+	node();
+	node(int value);
+	~node();
+	friend void gothrough(node *p);
+
+private:
+	node *left, *right; // the left child and the right child pointer
+	int number; // record the node's number
+	int is_threadl, is_threadr; //the flag to record whether the pointer is a thread or not
+
+	friend class op_tree_totum;//you could access all valuables in class op_tree_totum
+};
+
+//ctor
+node::node(){
+	left = right = NULL;
+	is_threadl = 1;
+	is_threadr = 1;
+}
+
+//ctor
+node::node(int value){
+	number = value;
+	left = right = NULL;
+	is_threadl = 1;
+	is_threadr = 1;
+}
+
+//dtor
+node::~node(){
+
+}
+
+class op_tree_totum{
+
+public:
+	op_tree_totum();
+	~op_tree_totum();
+	void insertion(int s);
+	void deletion(int s);
+	void inorder_run();
+	void reverseorder_run();
+	int size();
+
+private:
+	node *root, *head, *tail;
+	int num;//caculate how many nodes in the totum
+};
+
+//ctor
+op_tree_totum::op_tree_totum(){
+	head = new node();
+	tail = new node();
+	head->right = tail; //initialize the head node to connect to the tail node
+	tail->left = head;
+	root = NULL;
+	num = 0;
+}
+
+//dtor
+op_tree_totum::~op_tree_totum(){
+	node *p = root;
+	if(p!=NULL)	gothrough(p);
+	num = 0;
+	if (head != NULL)delete head;
+	if (tail != NULL)delete tail;
+}
+
+void gothrough(node *p){
+	if (p->is_threadl==0 && p->left!= NULL) gothrough(p->left);
+	if (p->is_threadr==0 && p->right!= NULL) gothrough(p->right);
+	delete p;
+}
+
+void op_tree_totum::insertion(int s){
+	//TODO: fill in the code to do the insertion of the node with number s
+
+
+
+
+}
+
+void op_tree_totum::deletion(int s){
+	//TODO: fill in the code to do the deletion of the node with number s
+
+
+
+}
+
+void op_tree_totum::inorder_run(){
+	//TODO: travel the tree from the head node to the tail node and output the values
+	//You should NOT use stack or recurtion 
+
+}
+
+void op_tree_totum::reverseorder_run(){
+	//TODO: travel the tree from the tail node to the head node and output the values
+	//You should NOT use stack or recurtion 
+
+}
+
+int op_tree_totum::size(){
+	return num;
+}
